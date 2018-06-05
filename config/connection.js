3f25204506739cf645cdbac93ae1b87f4ed9073a
Edward Finley <biged621@gmail.com>
2018-06-05 17:11:18 -0400

    screw this

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
-
-let connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-04.cleardb.net",
+let connection;
+if (process.env.CLEARDB_DATABASE_URL) {
+  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
+} else {
+connection = mysql.createConnection({
+  host: "localhost",
   port: 3306,
-  user: "bee747c0cbee0e",
-  password: "34570445",
-  database: "heroku_825b14b8ea9a46b"
+  user: "root",
+  password: "HamburglarMcNasty92",
+  database: "commerce_db"
 });
+}
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
