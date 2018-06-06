dcbbe74c1014b54fee55b1ca096cddd3b39eaaa8
Ramzan Ali <purvam98@gmail.com>
2018-06-05 21:52:07 -0400

    eg

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,9 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
 var connection;
-var JAWSDB_URL="mysql://m8s4cswrtyk8pr2n:lstlhwlmihvohzz4@nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rziwiqa48l991pia";
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+} else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
